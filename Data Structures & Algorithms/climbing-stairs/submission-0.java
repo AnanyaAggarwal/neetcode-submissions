8d3f3d21af72bb14586608f2af6bdb0d9394794c
Ananya Aggarwal <ananyaaggarwal3@gmail.com>
2026-06-28 17:18:34 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return climbStairs(n, dp); 
+    }
+
+    private int climbStairs(int n, int[] dp) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        dp[n] = climbStairs(n-1, dp) + climbStairs(n-2, dp);
+        return dp[n];
+    }
+}
