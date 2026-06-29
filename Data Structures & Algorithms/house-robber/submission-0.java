a6cec38a3d6fa7bdcb375395ed7165aff9c9d474
Ananya Aggarwal <ananyaaggarwal3@gmail.com>
2026-06-29 15:10:08 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length + 1];
+        Arrays.fill(dp, Integer.MIN_VALUE);
+    
+        return maxSum(nums.length - 1, nums, dp);
+    }
+
+    private int maxSum(int idx, int[] nums, int[] dp) {
+        if (idx == 0) {
+            return nums[0];
+        }
+        if (idx < 0) {
+            return 0;
+        }
+
+        if (dp[idx] != Integer.MIN_VALUE) {
+            return dp[idx];
+        }
+
+        int pick = nums[idx] + maxSum(idx-2, nums, dp);
+        int notPick = 0 + maxSum(idx-1, nums, dp);
+        dp[idx] = Math.max(pick, notPick);
+
+        return dp[idx];
+    }
+}
